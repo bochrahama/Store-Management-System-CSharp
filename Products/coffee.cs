fb2ea458630198f3562afc5aba29098cbca45a7d
Bochra <Bochrahiba@outlook.com>
2026-06-30 09:40:13 +0100

    Update coffee class

--- Products/coffee.cs
+++ Products/coffee.cs
@@ -1,4 +1,12 @@
 namespace StoreManagementSystem.Products{
+    public enum TypeCoffee
+    {
+        Espresso,
+        Latte,
+        Cappuccino,
+        Americano,
+        Macchiato       
+    }
     public enum Size
     {
         Small , Medium , Large
@@ -9,13 +17,14 @@ namespace StoreManagementSystem.Products{
     }
     public class Coffee : Product
     {
+        public  TypeCoffee type {get; set;}
         public Size Size {get ; set ;}
         public CaffeineLevel CaffeineLevel {get ; set ;}
         public bool isIced {get ; set;}
 
-        public Coffee (int productId ,  string name , decimal price , int quantity,
+        public Coffee (int productId ,  TypeCoffee type , decimal price , int quantity,
                     Size size , CaffeineLevel caffeineLevel ,bool isIced )
-                    : base (productId ,name , price , quantity )
+                    : base (productId ,type.ToString() , price , quantity )
         {
             this.Size = size ;
             this.CaffeineLevel = caffeineLevel ; 
