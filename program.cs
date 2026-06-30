333007f88add1320eede14a5b33e050a526ddb24
Bochra <Bochrahiba@outlook.com>
2026-06-30 09:59:11 +0100

    update the main program

--- program.cs
+++ program.cs
@@ -1,5 +1,10 @@
-﻿using StoreManagementSystem.Products;
+﻿using System.Reflection.PortableExecutable;
+using StoreManagementSystem.Products;
 
 
-Coffee cof = new Coffee(4, "Espresso", 30.4m, 4, Size.Small, CaffeineLevel.Medium, true);
+Coffee cof = new Coffee(4, TypeCoffee.Espresso, 30.4m, 4, Size.Small, CaffeineLevel.Medium, true);
 Console.WriteLine(cof);
+
+
+Tea tea = new Tea(5, TypeTea.Green, 30.4m, 4, 3, true);
+Console.WriteLine(tea);
