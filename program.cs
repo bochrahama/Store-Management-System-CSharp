f9dcf12cc3ac35aad2dfa85fb5ab76075aeae1a9
Bochra <Bochrahiba@outlook.com>
2026-06-30 08:20:23 +0100

    Update main program logic

---
+++ program.cs
@@ -0,0 +1,4 @@
+﻿using StoreManagementSystem.Products;
+
+// الآن يمكنكِ إنشاء كائن القهوة هنا وتجربته بنجاح:
+Coffee cof = new Coffee(4, "Espresso", 30.4m, 4, Size.Small, CaffeineLevel.Medium, true);
