8414c122ef4d48f791cb5be71a9b4ee17a354f37
Bochra <Bochrahiba@outlook.com>
2026-07-06 15:40:36 +0100

    complite Category class (with list )

--- Products/Category.cs
+++ Products/Category.cs
@@ -6,6 +6,13 @@ namespace  StoreManagementSystem.Products
 {
    public class Category
     {
+        public int  CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+         
+        public Category (int CategoryID, string CategoryName , string Description) 
+        {
 
+        }
     }
 }
