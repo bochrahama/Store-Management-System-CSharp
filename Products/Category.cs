67782b1f74490ff4882700f0d254c71a68b0cd7f
Bochra <Bochrahiba@outlook.com>
2026-07-06 15:51:15 +0100

    update the Category class

--- Products/Category.cs
+++ Products/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace  StoreManagementSystem.Products
@@ -9,10 +10,23 @@ namespace  StoreManagementSystem.Products
         public int  CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
-         
+        
+        private List<Product> _products = new List<Product>();
         public Category (int CategoryID, string CategoryName , string Description) 
         {
-
+            this.CategoryID = CategoryID;
+            this.CategoryName = CategoryName;
+            this.Description = Description;
         }
+        public void AddProduct(Product product)
+        {
+            if (product!= null && _products.Contains(product))
+            
+               _products.Add(product); 
+        }
+         public List<Product> GetProductsCategory(){
+                return new List<Product>(_products);
+            }
     }
 }
+
